3b25940838e1f2518c12cd7e85eaf5ed37d0ce7c
kiko923 <2725925825@qq.com>
2025-08-21 02:57:26 +0800

    Update [shortKey].js

--- functions/[shortKey].js
+++ functions/[shortKey].js
@@ -15,5 +15,7 @@ export async function onRequest(context) {
   } else {
     // 如果找不到，返回 404
     return new Response("该短链接已失效 请重新生成 生成后将永久生效", { status: 404 });
+    // 未找到 → 回首页并带上标记
+    // return Response.redirect(`/?error=notfound`, 302);
   }
 }
